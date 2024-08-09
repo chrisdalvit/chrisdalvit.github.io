4da239efed0be5a98461236e7eafc71b65e2360d
Christian Dalvit <chris.dalvit@gmail.com>
2024-08-09 22:35:30 +0200

    setup build

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	base: "/chrisdalvit.github.io/",
 	plugins: [sveltekit()]
 });
