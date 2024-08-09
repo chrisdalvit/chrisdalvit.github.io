19c553e48a5785c2c2f60d9b5854133667332b6a
Christian Dalvit <chris.dalvit@gmail.com>
2024-08-09 22:35:18 +0200

    simplify structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,26 @@
 <script>
-    import ArticleTilesContainer from "$lib/ArticleTilesContainer.svelte";    
+    import ArticleTile from "$lib/ArticleTile.svelte" 
 </script>
 
 
 <h1>Hi!</h1>
-<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-<ArticleTilesContainer />
+<p>
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+    when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+    containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+    including versions of Lorem Ipsum.
+</p>
+<div>
+    <ArticleTile article={{
+        title: "Sodoku Solver",
+        description: "Theory of my Sudoku solver project",
+        image: "images/cat1.jpeg",
+        url: ""
+    }} /> 
+</div>
 
 
 <style>
