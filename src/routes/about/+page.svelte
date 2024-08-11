294e597f79bdff14fac8147806b1db69aaace2ef
Christian Dalvit <chris.dalvit@gmail.com>
2024-08-11 12:51:31 +0200

    add about me page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,38 @@
-<h1>About</h1>
-<p>TODO: implement page</p>
+<div class="content">
+    <div class="article">
+        <h1>About Me</h1>
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            It has survived not only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
+            containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
+            including versions of Lorem Ipsum.
+        </p>
+    </div>
+</div>
+
+<style>
+    h1 {
+        font-family: "Sometype Mono", monospace;
+        font-optical-sizing: auto;
+        font-style: normal;
+    }
+
+    p {
+        text-align: justify;
+        line-height: 1.5;
+        margin-bottom: 54px;
+    }
+
+    div.content {
+        display: grid;
+        flex-direction: row;
+    }
+
+    div.article {
+        max-width: 700px;
+        padding: 16px;
+    }
+</style>
