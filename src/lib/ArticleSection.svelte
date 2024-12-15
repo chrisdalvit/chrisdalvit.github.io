244adf72323d6a32ac9e52afb040ba220e4b723a
Christian Dalvit <chris.dalvit@gmail.com>
2024-12-15 15:57:22 +0100

    fix css style

--- src/lib/ArticleSection.svelte
+++ src/lib/ArticleSection.svelte
@@ -50,6 +50,15 @@
         text-decoration-color: #63a563;
     }
 
+    :global(li) {
+        font-family: "Heebo", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 300;
+        font-style: normal;
+        text-align: justify;
+        line-height: 1.7rem;
+    }
+
     .section-title {
         font-family: "Sometype Mono", monospace;
         font-optical-sizing: auto;
