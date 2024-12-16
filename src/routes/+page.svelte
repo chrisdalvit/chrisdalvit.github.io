79a5bda852aea8636e2c5f06d0c4ac7abd44d816
Christian Dalvit <chris.dalvit@gmail.com>
2024-12-16 23:39:04 +0100

    Add intro text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,13 +8,7 @@
     <div class="overview" slot="center">
         <h1 class="title">Hi!</h1>
         <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-            It has survived not only five centuries, but also the leap into electronic typesetting, 
-            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets 
-            containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
-            including versions of Lorem Ipsum.
+            I'm Christian. Somehow, you've landed on my small corner of the internet. Here, I share my projects and thoughts about stuff that I'm intrested in. Feel free to explore and connect with me!
         </p>
         <ArticleTile article={{
             title: "Sodoku Solver",
