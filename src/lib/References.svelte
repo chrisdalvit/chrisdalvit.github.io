d95af7148010ae59da9193d7ac767b50086b6650
Christian Dalvit <chris.dalvit@gmail.com>
2024-12-18 11:56:07 +0100

    add citation and references components

---
+++ src/lib/References.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+
+    type CitationType = {
+        index: number,
+        author: string,
+        title: string,
+        year: string,
+        link: string
+    }
+    export let citations: Record<string, CitationType>
+</script>
+
+
+<div>
+    <h3 id="references" class="section-title">References</h3> 
+    {#each Object.entries(citations) as [tag, citation]}
+        <p>[{citation.index}] <a href={citation.link} target="_blank">{citation.author} "{citation.title}", {citation.year}</a></p>
+    {/each}
+</div>
+
+<style>
+    .section-title {
+        font-family: "Sometype Mono", monospace;
+        font-optical-sizing: auto;
+        font-style: normal;
+        text-decoration: none;
+        color: black;
+        font-weight: bold;
+        font-size: 1.5em;
+        margin-top: 40px;
+    }
+</style>
