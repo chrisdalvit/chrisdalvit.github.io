f53c6eb5df4072e9b64fb1d426fb3de4ddab96ad
Christian Dalvit <chris.dalvit@gmail.com>
2024-12-20 00:17:34 +0100

    add email address

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -18,7 +18,7 @@
             If you'd like to connect, feel free to reach out via email, or you can find me on GitHub and LinkedIn.
         </p>
         <div>
-            <a class="icon-container" href=" ">
+            <a class="icon-container" href="mailto:chrisdalvit.blog@gmail.com">
                 <Icon icon="mdi:email-outline" width=48 height=48 />
             </a>
             <a class="icon-container" href="https://github.com/chrisdalvit" target="_blank">
